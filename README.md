f299154b141873326c40a74f603f899bc1528af5
jts272 <jts272@outlook.com>
2023-02-11 01:03:06 +0000

    Complete lesson content

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 ~~- In your own repository, click the green "Gitpod" button~~
 
 Template used to create new repository. Repo then cloned from url to local folder.
+
+---
+
+Rest parameter syntax works in the opposite way of the spread operator.
+
+- spread breaks arrays into individual elements to be copied
+- rest combines individual elements into an array
+- can also be used to represent the 'rest' of the values when destructuring an array or object
--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -1,10 +1,27 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node rest-parameter-syntax.js in the terminal
  */
 
-// Regular function call 
+// Regular function call
+const sumAll = (a, b, c) => a + b + c;
+console.log("Sum is: ", sumAll(1, 2, 3));
 
 // Extra arguments are ignored
+console.log("Sum is: ", sumAll(1, 2, 3, 4, 5, 6));
+// Sum is still 6 because the extra args are ignored
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+  // '...rest' allows aargs. Doesn't have to be called rest strictly.
+  let sum = a + b + c; // <-- add a, b and c like normal
+  for (let i of rest) {
+    // loop thru whatever is given for the 'rest array'
+    sum += i; // <-- add these to the sum var
+  }
+
+  return sum;
+};
+
+console.log(sumRest(1, 2, 3, 4, 5, 6));
+// output as expected
